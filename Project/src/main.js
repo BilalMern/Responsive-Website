2d39fe81c1c64776f9762a5cdf3bf1f5c9918fcd
BilalMern <bilal.mern@gmail.com>
2024-05-13 14:46:26 +0500

    Responsive breakpoints

--- Project/src/main.js
+++ Project/src/main.js
@@ -1,11 +1,14 @@
 let openBtn = document.querySelector(".bi-list");
 let closeBtn = document.querySelector(".bi-x-lg");
+let menu = document.querySelector(".navbar__menu");
 openBtn.addEventListener("click", () => {
   openBtn.classList.toggle("hide"); //By this on click the class "hide" get attached to open button.
   closeBtn.classList.toggle("hide");
+  menu.classList.toggle("hidden");
 });
 
 closeBtn.addEventListener("click", () => {
-    openBtn.classList.toggle("hide"); //By this on click the class "hide" get attached to open button.
-    closeBtn.classList.toggle("hide");
-  });
+  openBtn.classList.toggle("hide"); //By this on click the class "hide" get attached to open button.
+  closeBtn.classList.toggle("hide");
+  menu.classList.toggle("hidden");
+});
